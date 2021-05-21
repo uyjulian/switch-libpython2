973de5dd6aa41736d41249227859201ff342a7a3
uyjulian <uyjulian@gmail.com>
2021-05-21 16:39:43 -0500

    Update devkita64 docker image

--- setup-docker.bash
+++ setup-docker.bash
@@ -1,6 +1,4 @@
 
-docker run -d --name switchdev --workdir /build/git -v "${PWD}:/build/git" devkitpro/devkita64:20210306 tail -f /dev/null
-curl -LOC - https://github.com/uyjulian/pacman-packages/releases/download/v2.2.3-1-pkgbuild-helpers/devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
-docker exec switchdev /bin/bash -c 'dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz'
+docker run -d --name switchdev --workdir /build/git -v "${PWD}:/build/git" devkitpro/devkita64:20210514 tail -f /dev/null
 docker exec switchdev /bin/bash -c 'apt-get -y update'
 docker exec switchdev /bin/bash -c 'apt-get -y install python libc6-dev'
